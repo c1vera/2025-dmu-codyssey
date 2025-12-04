5517b4db8b0117111636e13ab68473db3007b02b
c1vera <yahwa2009@naver.com>
2025-12-04 17:28:12 +0900

    feat: 4-7

--- part4/mission1/mission5/database.py
+++ part4/mission1/mission5/database.py
@@ -1,3 +1,4 @@
+import contextlib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -11,6 +12,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+@contextlib.contextmanager
 def get_db():
     db = SessionLocal()
     try:
--- part4/mission1/mission5/domain/question/question_router.py
+++ part4/mission1/mission5/domain/question/question_router.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import ContextManager
 
 from database import get_db
 from models import Question
+from domain.question import question_schema
 
 router = APIRouter(
     prefix="/api/question",
 )
 
-@router.get("/list")
-def question_list(db: Session = Depends(get_db)):
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    return _question_list
-
+@router.get("/list", response_model=list[question_schema.Question])
+def question_list(db_context: ContextManager[Session] = Depends(get_db)):
+    with db_context as db:
+        _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+        return _question_list
